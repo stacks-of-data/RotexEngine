35619769236a079f6a6c6902dfca5bc9b6ec659c
stacks-of-data <coder64bit@gmail.com>
2026-01-02 00:09:25 +0300

    SDL Window created

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
 # RotexEngine
+
+Graphics engine built on top of Vulkan and SDL3 that aims to be modern, performant and efficient with rich features.
+
+## TODO
+- [ ] Setup Vulkan swapchain
+  - [x] Setup window context with SDL.
+  - [ ] Prepare Validation Layers.
+  - [ ] Create Vulkan Instance.
+  - [ ] Setup Debug Messenger.
+  - [ ] Create Surface.
+  - [ ] Pick Physical Device & Find Queue Families.
+  - [ ] Create Logical Device.
+  - [ ] Create Vulkan Swapchain.
--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,37 @@
+#include "SDL3/SDL_events.h"
+#include "SDL3/SDL_init.h"
+#include "SDL3/SDL_log.h"
+#include "SDL3/SDL_oldnames.h"
+#include "SDL3/SDL_pixels.h"
+#include "SDL3/SDL_rect.h"
+#include "SDL3/SDL_render.h"
+#include "SDL3/SDL_video.h"
 #include <iostream>
 
 int main(int argc, char *argv[]) {
-  std::cout << "Hello, World!" << std::endl;
+  SDL_Window *window = nullptr;
+  if (SDL_InitSubSystem(SDL_INIT_VIDEO) == false) {
+    std::cout << "SDL_Init Error: " << SDL_GetError() << std::endl;
+    return 1;
+  }
+  SDL_Log("Hello, World!");
+  window = SDL_CreateWindow("RotexEngine", 800, 800, SDL_WINDOW_VULKAN);
+  if (window == nullptr) {
+    std::cout << "SDL_CreateWindow Error: " << SDL_GetError() << std::endl;
+    SDL_QuitSubSystem(SDL_INIT_VIDEO);
+    SDL_Quit();
+    return 1;
+  }
+  SDL_Event e;
+  bool quit = false;
+  while (quit == false) {
+    while (SDL_PollEvent(&e)) {
+      if (e.type == SDL_EVENT_QUIT)
+        quit = true;
+    }
+  }
+  SDL_DestroyWindow(window);
+  SDL_QuitSubSystem(SDL_INIT_VIDEO);
+  SDL_Quit();
   return 0;
 }
